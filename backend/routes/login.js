10777709f971d9bd05b7f18bbde65f8b1eadcd26
Qwerty12305 <jodhasingh345646@gmail.com>
2026-08-17 19:41:25 +0530

    Fix Gmail SMTP connection

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -272,15 +272,21 @@ const verifyCaptcha = (
 |--------------------------------------------------------------------------
 */
 
-const transporter =
-  nodemailer.createTransport({
-    service: "gmail",
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+});
 
 /*
 |--------------------------------------------------------------------------
